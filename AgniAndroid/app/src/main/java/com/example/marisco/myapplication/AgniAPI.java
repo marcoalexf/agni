2b822cbbf5bea485e68b781856f72c40722556e2
Marco Francisco <marco.a.f@live.com.pt>
2018-06-20 12:27:01 +0100

    OMG PHOTO UPLOAD WORKS NIGGA

--- AgniAndroid/app/src/main/java/com/example/marisco/myapplication/AgniAPI.java
+++ AgniAndroid/app/src/main/java/com/example/marisco/myapplication/AgniAPI.java
@@ -4,13 +4,14 @@ import java.io.File;
 import java.util.List;
 import java.util.Map;
 
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
-import retrofit2.http.Query;
 
 public interface AgniAPI {
 
@@ -33,7 +34,7 @@ public interface AgniAPI {
     Call<MediaUploadResponse> registerOccurrencePhoto(@Body OccurrenceData data);
 
     @POST("upload/{id}")
-    Call<ResponseBody> uploadPhoto(@Path("id") Long l, @Body byte[] file);
+    Call<ResponseBody> uploadPhoto(@Path("id") Long l, @Header("Content-Type") String contentType, @Body RequestBody file);
 
     @GET("occurrence/list")
     Call<CursorList > getOccurrences();
